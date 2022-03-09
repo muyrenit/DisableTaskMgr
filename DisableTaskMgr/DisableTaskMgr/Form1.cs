6661f445d7ce9aa3551ec006a1ce39626b549acd
muyrenit <74090232+muyrenit@users.noreply.github.com>
2022-03-09 19:06:50 +0300

    Add files via upload

---
+++ DisableTaskMgr/DisableTaskMgr/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace DisableTaskMgr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetValue("DisableTaskMgr", 1);
+        }
+
+        private void SetValue(string value, object data)
+        {
+            Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System", value, data);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetValue("DisableTaskMgr", 0);
+        }
+    }
+}
